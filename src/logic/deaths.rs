c4910130b7a350206a4230ed2fbd2fc2815a8bf1
kuviman <kuviman@gmail.com>
2022-03-04 04:58:38 +0400

    Refactor process_deaths

--- src/logic/deaths.rs
+++ src/logic/deaths.rs
@@ -2,16 +2,15 @@ use super::*;
 
 impl Game {
     pub fn process_deaths(&mut self) {
-        for id in self.units.ids().copied().collect::<Vec<Id>>() {
-            let mut unit = self.units.remove(&id).unwrap();
-            if unit.hp <= 0 {
-                let effects = unit.death_effects.clone();
-                for effect in effects {
-                    self.apply_effect(&effect, None, &mut unit);
-                }
-            }
-            self.units.insert(unit);
-        }
+        self.process_units(Self::process_unit_death);
         self.units.retain(|unit| unit.hp > 0);
+    }
+    fn process_unit_death(&mut self, unit: &mut Unit) {
+        if unit.hp <= 0 {
+            let effects = unit.death_effects.clone();
+            for effect in effects {
+                self.apply_effect(&effect, None, unit);
+            }
+        }
     }
 }
