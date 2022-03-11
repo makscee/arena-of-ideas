795ee191e751ab3d5d22751c35e683150bd56eba
kuviman <kuviman@gmail.com>
2022-03-11 10:27:24 +0400

    Simplify modifiers

--- src/logic/effects/modifiers/mod.rs
+++ src/logic/effects/modifiers/mod.rs
@@ -9,41 +9,10 @@ pub struct WeakenModifier {
 impl WeakenModifier {
     pub fn apply(&self, effect: &mut Effect) {
         let multiplier = R32::ONE - self.percent / r32(100.0);
-        match effect {
-            Effect::Noop => {}
-            Effect::Damage(effect) => match &mut effect.hp {
-                DamageValue::Absolute(value) => *value *= multiplier,
-                DamageValue::Relative(value) => *value *= multiplier,
-            },
-            Effect::Projectile(effect) => {
-                self.apply(&mut effect.effect);
-            }
-            Effect::AddStatus(_) => {}
-            Effect::Spawn(_) => {}
-            Effect::AOE(effect) => {
-                self.apply(&mut effect.effect);
-            }
-            Effect::TimeBomb(effect) => {
-                self.apply(&mut effect.effect);
-            }
-            Effect::Suicide(_) => {}
-            Effect::Chain(effect) => {
-                self.apply(&mut effect.effect);
-            }
-            Effect::Repeat { effect, .. } => {
-                self.apply(effect);
-            }
-            Effect::List { effects } => {
-                for effect in effects {
-                    self.apply(effect);
-                }
-            }
-            Effect::Random { choices } => {
-                for choice in choices {
-                    self.apply(&mut choice.effect);
-                }
-            }
-        }
+        effect.walk_mut(&mut |effect| match effect {
+            Effect::Damage(damage) => damage.hp = damage.hp * multiplier,
+            _ => {}
+        });
     }
 }
 
