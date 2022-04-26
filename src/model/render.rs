d0327aaf4a7eb0d2273ef12eb13cb3146cdf429f
Nertsal <sasha.kudasov04@gmail.com>
2022-04-26 11:50:49 +0300

    Different types for shader parameter

--- src/model/render.rs
+++ src/model/render.rs
@@ -47,7 +47,7 @@ impl Default for RenderMode {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
-pub struct ShaderParameters(HashMap<String, f32>);
+pub struct ShaderParameters(HashMap<String, ShaderParameter>);
 
 impl ugli::Uniforms for ShaderParameters {
     fn walk_uniforms<C>(&self, visitor: &mut C)
@@ -56,6 +56,30 @@ impl ugli::Uniforms for ShaderParameters {
     {
         for (name, value) in &self.0 {
             visitor.visit(name, value);
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum ShaderParameter {
+    Int(i32),
+    Float(f32),
+    Vec2(Vec2<f32>),
+    Vec3(Vec3<f32>),
+    Vec4(Vec4<f32>),
+    Color(Color<f32>),
+}
+
+impl ugli::Uniform for ShaderParameter {
+    fn apply(&self, gl: &ugli_raw::Context, info: &ugli::UniformInfo) {
+        match self {
+            Self::Int(value) => value.apply(gl, info),
+            Self::Float(value) => value.apply(gl, info),
+            Self::Vec2(value) => value.apply(gl, info),
+            Self::Vec3(value) => value.apply(gl, info),
+            Self::Vec4(value) => value.apply(gl, info),
+            Self::Color(value) => value.apply(gl, info),
         }
     }
 }
