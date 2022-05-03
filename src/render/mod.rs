6fbabcae116f4b5196d70f54a1017a782aeac216
Nertsal <sasha.kudasov04@gmail.com>
2022-05-03 23:17:56 +0300

    Extract UnitRender from Render

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -41,6 +41,12 @@ pub struct Render {
     geng: Geng,
     camera: geng::Camera2d,
     assets: Rc<Assets>,
+    unit_render: UnitRender,
+}
+
+pub struct UnitRender {
+    geng: Geng,
+    assets: Rc<Assets>,
 }
 
 impl Render {
@@ -53,6 +59,7 @@ impl Render {
                 rotation: 0.0,
                 fov: config.fov,
             },
+            unit_render: UnitRender::new(geng, assets),
         }
     }
     pub fn draw(
@@ -127,135 +134,15 @@ impl Render {
         game_time: f32,
         framebuffer: &mut ugli::Framebuffer,
     ) {
-        let spawn_scale = match unit.spawn_animation_time_left {
-            Some(time) if template.spawn_animation_time > Time::new(0.0) => {
-                1.0 - (time / template.spawn_animation_time).as_f32()
-            }
-            _ => 1.0,
-        };
-        let attack_scale = match &unit.action_state {
-            ActionState::Start { time, .. } => {
-                1.0 - 0.25 * (*time / unit.action.animation_delay).as_f32()
-            }
-            _ => 1.0,
-        };
-
-        match render_mode {
-            RenderMode::Circle { color } => {
-                self.geng.draw_2d(
-                    framebuffer,
-                    &self.camera,
-                    &draw_2d::Ellipse::circle(
-                        unit.position.map(|x| x.as_f32()),
-                        unit.radius.as_f32() * attack_scale * spawn_scale,
-                        {
-                            let mut color = *color;
-                            if unit
-                                .all_statuses
-                                .iter()
-                                .any(|status| status.r#type() == StatusType::Freeze)
-                            {
-                                color = Color::CYAN;
-                            }
-                            if unit
-                                .all_statuses
-                                .iter()
-                                .any(|status| matches!(status, Status::Slow { .. }))
-                            {
-                                color = Color::GRAY;
-                            }
-                            color
-                        },
-                    ),
-                );
-            }
-            RenderMode::Texture { texture } => {
-                self.geng.draw_2d(
-                    framebuffer,
-                    &self.camera,
-                    &draw_2d::TexturedQuad::unit(&**texture)
-                        .scale_uniform(unit.radius.as_f32() * attack_scale * spawn_scale)
-                        .translate(unit.position.map(|x| x.as_f32())),
-                );
-            }
-            RenderMode::Shader {
-                program,
-                parameters,
-            } => {
-                let quad = ugli::VertexBuffer::new_dynamic(
-                    self.geng.ugli(),
-                    vec![
-                        draw_2d::Vertex {
-                            a_pos: vec2(-1.0, -1.0),
-                        },
-                        draw_2d::Vertex {
-                            a_pos: vec2(1.0, -1.0),
-                        },
-                        draw_2d::Vertex {
-                            a_pos: vec2(1.0, 1.0),
-                        },
-                        draw_2d::Vertex {
-                            a_pos: vec2(-1.0, 1.0),
-                        },
-                    ],
-                );
-                let framebuffer_size = framebuffer.size();
-                let model_matrix = Mat3::translate(unit.position.map(|x| x.as_f32()))
-                    * Mat3::scale_uniform(unit.radius.as_f32() * attack_scale * spawn_scale);
-
-                let mut alliances: Vec<Alliance> = unit.alliances.iter().copied().collect();
-                let alliance_colors: Vec<Color<f32>> = alliances
-                    .iter()
-                    .map(|alliance| self.assets.options.alliance_colors[alliance])
-                    .collect();
-
-                let (action_time, target) = match &unit.action_state {
-                    ActionState::Start { time, target } => (
-                        (*time / unit.action.animation_delay).as_f32(),
-                        model.units.get(&target).map(|unit| unit),
-                    ),
-                    _ => (0.0, None),
-                };
-
-                let target_dir = target
-                    .map_or(Vec2::ZERO, |target| {
-                        (target.position - unit.position).normalize_or_zero()
-                    })
-                    .map(|x| x.as_f32());
-
-                ugli::draw(
-                    framebuffer,
-                    program,
-                    ugli::DrawMode::TriangleFan,
-                    &quad,
-                    (
-                        ugli::uniforms! {
-                            u_time: game_time,
-                            u_unit_position: unit.position.map(|x| x.as_f32()),
-                            u_unit_radius: unit.radius.as_f32(),
-                            u_spawn: spawn_scale,
-                            u_action: action_time,
-                            u_cooldown: unit.action.cooldown.as_f32(),
-                            u_animation_delay: unit.action.animation_delay.as_f32(),
-                            u_target_dir: target_dir,
-                            u_random: unit.random_number.as_f32(),
-                            u_action_time: unit.last_action_time.as_f32(),
-                            u_injure_time: unit.last_injure_time.as_f32(),
-                            u_alliance_color_1: alliance_colors.get(0).copied().unwrap_or(Color::WHITE),
-                            u_alliance_color_2: alliance_colors.get(1).copied().unwrap_or(Color::WHITE),
-                            u_alliance_color_3: alliance_colors.get(2).copied().unwrap_or(Color::WHITE),
-                            u_alliance_count: alliance_colors.len(),
-                        },
-                        geng::camera2d_uniforms(&self.camera, framebuffer_size.map(|x| x as f32)),
-                        parameters,
-                    ),
-                    ugli::DrawParameters {
-                        blend_mode: Some(default()),
-                        ..default()
-                    },
-                );
-            }
-        }
+        self.unit_render.draw_unit(
+            unit,
+            template,
+            render_mode,
+            model,
+            game_time,
+            &self.camera,
+            framebuffer,
+        );
     }
 
     fn draw_particle(
@@ -329,6 +216,156 @@ impl Render {
                             u_alliance_count: 0,
                         },
                         geng::camera2d_uniforms(&self.camera, framebuffer_size.map(|x| x as f32)),
+                        parameters,
+                    ),
+                    ugli::DrawParameters {
+                        blend_mode: Some(default()),
+                        ..default()
+                    },
+                );
+            }
+        }
+    }
+}
+
+impl UnitRender {
+    pub fn new(geng: &Geng, assets: &Rc<Assets>) -> Self {
+        Self {
+            geng: geng.clone(),
+            assets: assets.clone(),
+        }
+    }
+
+    pub fn draw_unit(
+        &self,
+        unit: &Unit,
+        template: &UnitTemplate,
+        render_mode: &RenderMode,
+        model: &Model,
+        game_time: f32,
+        camera: &impl geng::AbstractCamera2d,
+        framebuffer: &mut ugli::Framebuffer,
+    ) {
+        let spawn_scale = match unit.spawn_animation_time_left {
+            Some(time) if template.spawn_animation_time > Time::new(0.0) => {
+                1.0 - (time / template.spawn_animation_time).as_f32()
+            }
+            _ => 1.0,
+        };
+        let attack_scale = match &unit.action_state {
+            ActionState::Start { time, .. } => {
+                1.0 - 0.25 * (*time / unit.action.animation_delay).as_f32()
+            }
+            _ => 1.0,
+        };
+
+        match render_mode {
+            RenderMode::Circle { color } => {
+                self.geng.draw_2d(
+                    framebuffer,
+                    camera,
+                    &draw_2d::Ellipse::circle(
+                        unit.position.map(|x| x.as_f32()),
+                        unit.radius.as_f32() * attack_scale * spawn_scale,
+                        {
+                            let mut color = *color;
+                            if unit
+                                .all_statuses
+                                .iter()
+                                .any(|status| status.r#type() == StatusType::Freeze)
+                            {
+                                color = Color::CYAN;
+                            }
+                            if unit
+                                .all_statuses
+                                .iter()
+                                .any(|status| matches!(status, Status::Slow { .. }))
+                            {
+                                color = Color::GRAY;
+                            }
+                            color
+                        },
+                    ),
+                );
+            }
+            RenderMode::Texture { texture } => {
+                self.geng.draw_2d(
+                    framebuffer,
+                    camera,
+                    &draw_2d::TexturedQuad::unit(&**texture)
+                        .scale_uniform(unit.radius.as_f32() * attack_scale * spawn_scale)
+                        .translate(unit.position.map(|x| x.as_f32())),
+                );
+            }
+            RenderMode::Shader {
+                program,
+                parameters,
+            } => {
+                let quad = ugli::VertexBuffer::new_dynamic(
+                    self.geng.ugli(),
+                    vec![
+                        draw_2d::Vertex {
+                            a_pos: vec2(-1.0, -1.0),
+                        },
+                        draw_2d::Vertex {
+                            a_pos: vec2(1.0, -1.0),
+                        },
+                        draw_2d::Vertex {
+                            a_pos: vec2(1.0, 1.0),
+                        },
+                        draw_2d::Vertex {
+                            a_pos: vec2(-1.0, 1.0),
+                        },
+                    ],
+                );
+                let framebuffer_size = framebuffer.size();
+                let model_matrix = Mat3::translate(unit.position.map(|x| x.as_f32()))
+                    * Mat3::scale_uniform(unit.radius.as_f32() * attack_scale * spawn_scale);
+
+                let mut alliances: Vec<Alliance> = unit.alliances.iter().copied().collect();
+                let alliance_colors: Vec<Color<f32>> = alliances
+                    .iter()
+                    .map(|alliance| self.assets.options.alliance_colors[alliance])
+                    .collect();
+
+                let (action_time, target) = match &unit.action_state {
+                    ActionState::Start { time, target } => (
+                        (*time / unit.action.animation_delay).as_f32(),
+                        model.units.get(&target).map(|unit| unit),
+                    ),
+                    _ => (0.0, None),
+                };
+
+                let target_dir = target
+                    .map_or(Vec2::ZERO, |target| {
+                        (target.position - unit.position).normalize_or_zero()
+                    })
+                    .map(|x| x.as_f32());
+
+                ugli::draw(
+                    framebuffer,
+                    program,
+                    ugli::DrawMode::TriangleFan,
+                    &quad,
+                    (
+                        ugli::uniforms! {
+                            u_time: game_time,
+                            u_unit_position: unit.position.map(|x| x.as_f32()),
+                            u_unit_radius: unit.radius.as_f32(),
+                            u_spawn: spawn_scale,
+                            u_action: action_time,
+                            u_cooldown: unit.action.cooldown.as_f32(),
+                            u_animation_delay: unit.action.animation_delay.as_f32(),
+                            u_target_dir: target_dir,
+                            u_random: unit.random_number.as_f32(),
+                            u_action_time: unit.last_action_time.as_f32(),
+                            u_injure_time: unit.last_injure_time.as_f32(),
+                            u_alliance_color_1: alliance_colors.get(0).copied().unwrap_or(Color::WHITE),
+                            u_alliance_color_2: alliance_colors.get(1).copied().unwrap_or(Color::WHITE),
+                            u_alliance_color_3: alliance_colors.get(2).copied().unwrap_or(Color::WHITE),
+                            u_alliance_count: alliance_colors.len(),
+                        },
+                        geng::camera2d_uniforms(camera, framebuffer_size.map(|x| x as f32)),
                         parameters,
                     ),
                     ugli::DrawParameters {
