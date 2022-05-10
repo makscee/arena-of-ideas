555c650a71009dab1d90b1af5c5c274739e3dfeb
Nertsal <sasha.kudasov04@gmail.com>
2022-05-10 11:12:35 +0300

    [Shop] Mark description and alliances as todo

--- src/shop/render/card.rs
+++ src/shop/render/card.rs
@@ -24,11 +24,23 @@ const TIER_AABB: AABB<f32> = AABB {
     y_min: 0.92,
     y_max: 0.97,
 };
+const ALLIANCE_AABB: AABB<f32> = AABB {
+    x_min: 1.0 / CARD_SIZE_RATIO - 0.27,
+    x_max: 1.0 / CARD_SIZE_RATIO - 0.05,
+    y_min: 0.917,
+    y_max: 0.973,
+};
 const NAME_AABB: AABB<f32> = AABB {
     x_min: 0.21,
     x_max: 0.53,
     y_min: 0.52,
     y_max: 0.58,
+};
+const DESCRIPTION_AABB: AABB<f32> = AABB {
+    x_min: 0.04,
+    x_max: 1.0 / CARD_SIZE_RATIO - 0.04,
+    y_min: 0.17,
+    y_max: 0.47,
 };
 const CARD_BACKGROUND_COLOR: Color<f32> = Color {
     r: 0.1,
@@ -76,7 +88,6 @@ impl CardRender {
         let mut hero_aabb = card_aabb.extend_up(-TOP_SPACE * height);
         hero_aabb.y_min = hero_aabb.y_max - HERO_HEIGHT * height;
         let hero_aabb = hero_aabb;
-
         let mut temp_texture = ugli::Texture::new_with(
             self.geng.ugli(),
             hero_aabb.size().map(|x| x.ceil() as _),
@@ -86,7 +97,6 @@ impl CardRender {
             self.geng.ugli(),
             ugli::ColorAttachment::Texture(&mut temp_texture),
         );
-
         self.render.draw_unit(
             &card.unit,
             &card.template,
@@ -100,25 +110,13 @@ impl CardRender {
             &mut temp_framebuffer,
         );
 
-        // HP
-        let hp_aabb = HP_AABB
-            .map(|x| x * height)
-            .translate(card_aabb.bottom_left());
-
-        // Damage
-        let damage_aabb = DAMAGE_AABB
-            .map(|x| x * height)
-            .translate(card_aabb.bottom_left());
-
-        // Tier
-        let tier_aabb = TIER_AABB
-            .map(|x| x * height)
-            .translate(card_aabb.bottom_left());
-
-        // Name
-        let name_aabb = NAME_AABB
-            .map(|x| x * height)
-            .translate(card_aabb.bottom_left());
+        let layout = |aabb: AABB<f32>| aabb.map(|x| x * height).translate(card_aabb.bottom_left());
+        let hp_aabb = layout(HP_AABB);
+        let damage_aabb = layout(DAMAGE_AABB);
+        let tier_aabb = layout(TIER_AABB);
+        let alliance_aabb = layout(ALLIANCE_AABB);
+        let name_aabb = layout(NAME_AABB);
+        let description_aabb = layout(DESCRIPTION_AABB);
 
         // Render
         // Hero
@@ -158,6 +156,15 @@ impl CardRender {
         .fit_into(tier_aabb)
         .draw_2d(&self.geng, framebuffer, camera);
 
+        // Tier
+        draw_2d::Text::unit(
+            &**self.geng.default_font(),
+            format!("TODO: Alliances"),
+            Color::WHITE,
+        )
+        .fit_into(alliance_aabb)
+        .draw_2d(&self.geng, framebuffer, camera);
+
         // Name
         draw_2d::Text::unit(
             &**self.geng.default_font(),
@@ -165,6 +172,15 @@ impl CardRender {
             Color::WHITE,
         )
         .fit_into(name_aabb)
+        .draw_2d(&self.geng, framebuffer, camera);
+
+        // Description
+        draw_2d::Text::unit(
+            &**self.geng.default_font(),
+            format!("TODO: Description"),
+            Color::WHITE,
+        )
+        .fit_into(description_aabb)
         .draw_2d(&self.geng, framebuffer, camera);
     }
 }
