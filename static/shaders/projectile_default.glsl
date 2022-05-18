93a9b56f67c0a49627f97c78d87d2202f9a60d9c
Maksim Chugunov <shadeoflance@gmail.com>
2022-05-18 17:56:37 +0300

    Add Projectile default shader

---
+++ static/shaders/projectile_default.glsl
@@ -0,0 +1,26 @@
+#include <common.glsl>
+
+
+#ifdef VERTEX_SHADER
+out vec2 v_quad_pos;
+attribute vec2 a_pos;
+uniform mat3 u_projection_matrix;
+uniform mat3 u_view_matrix;
+void main() {
+    v_quad_pos = a_pos * (1.0 + padding);
+    float size = u_unit_radius * u_spawn * (1.0 - 0.25 * u_action);
+    vec2 pos = v_quad_pos * size + u_unit_position;
+    vec3 p_pos = u_projection_matrix * u_view_matrix * vec3(pos, 1.0);
+    gl_Position = vec4(p_pos.xy, 0.0, p_pos.z);
+}
+#endif
+
+#ifdef FRAGMENT_SHADER
+in vec2 v_quad_pos;
+
+void main() {
+    vec4 col = vec4(1);
+
+    gl_FragColor = col;
+}
+#endif
