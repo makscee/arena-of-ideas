24dff570309dc6a04bf7c89968d351ae89fd636c
Artem Povaliaev <gevernus@mail.ru>
2022-11-15 18:39:28 +0400

    Fix merge conflicts

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -134,6 +134,28 @@ impl Model {
             .expect(&format!("Can't find {}#{}", who, who_id))
     }
 
+    pub fn get_who_mut(&mut self, who: Who, context: &EffectContext) -> &mut Unit {
+        let who_id = context.get_id(who);
+        self.units
+            .get_mut(&who_id)
+            .or(self.dead_units.get_mut(&who_id))
+            .expect(&format!("Can't find {}#{}", who, who_id))
+    }
+
+    pub fn get(&self, id: Id, context: &EffectContext) -> &Unit {
+        self.units
+            .get(&id)
+            .or(self.dead_units.get(&id))
+            .expect(&format!("Can't find Unit#{}", id))
+    }
+
+    pub fn get_mut(&mut self, id: Id, context: &EffectContext) -> &mut Unit {
+        self.units
+            .get_mut(&id)
+            .or(self.dead_units.get_mut(&id))
+            .expect(&format!("Can't find Unit#{}", id))
+    }
+
     pub fn get_all(&self, context: &EffectContext) -> Vec<&Unit> {
         let mut result: Vec<&Unit> = vec![];
         self.units
@@ -150,41 +172,19 @@ impl Model {
         result
     }
 
-    pub fn get(&self, id: Id, context: &EffectContext) -> &Unit {
-        self.units
-            .get(&id)
-            .or(self.dead_units.get(&id))
-            .expect(&format!("Can't find Unit#{}", id))
-    }
-
-    pub fn get_mut(&mut self, id: Id, context: &EffectContext) -> &mut Unit {
-        self.units
-            .get_mut(&id)
-            .or(self.dead_units.get_mut(&id))
-            .expect(&format!("Can't find Unit#{}", id))
-    }
-    
-    pub fn get_who_mut(&mut self, who: Who, context: &EffectContext) -> &mut Unit {
-        let who_id = context.get_id(who);
-        self.units
-            .get_mut(&who_id)
-            .or(self.dead_units.get_mut(&who_id))
-            .expect(&format!("Can't find {}#{}", who, who_id))
-    }
-
     pub fn check_condition(&self, condition: &Condition, context: &EffectContext) -> bool {
         match condition {
             Condition::Always => true,
             Condition::Not { condition } => !self.check_condition(condition, context),
             Condition::UnitHasStatus { who, status_type } => {
-                let who = self.get(*who, &context);
+                let who = self.get_who(*who, &context);
                 who.all_statuses
                     .iter()
                     .any(|status| status.status.name == *status_type)
             }
             Condition::InRange { max_distance } => {
-                let owner = self.get(Who::Owner, &context);
-                let target = self.get(Who::Target, &context);
+                let owner = self.get_who(Who::Owner, &context);
+                let target = self.get_who(Who::Target, &context);
                 distance_between_units(owner, target) <= *max_distance
             }
             Condition::Chance { percent } => {
@@ -197,12 +197,12 @@ impl Model {
                 self.config.clans.contains_key(clan) && self.config.clans[clan] >= *count
             }
             Condition::HasClan { who, clan } => {
-                let who = self.get(*who, &context);
+                let who = self.get_who(*who, &context);
                 who.clans.contains(clan)
             }
             Condition::HasVar { name } => context.vars.contains_key(name),
             Condition::Faction { who, faction } => {
-                let who = self.get(*who, &context);
+                let who = self.get_who(*who, &context);
                 who.faction == *faction
             }
             Condition::And { a, b } => {
@@ -210,22 +210,10 @@ impl Model {
                     && Self::check_condition(self, &*b, context)
             }
             Condition::Position { who, position } => {
-                let who = self.get(*who, &context);
+                let who = self.get_who(*who, &context);
                 who.position.x == *position
             }
         }
-    pub fn get(&self, id: Id, context: &EffectContext) -> &Unit {
-        self.units
-            .get(&id)
-            .or(self.dead_units.get(&id))
-            .expect(&format!("Can't find Unit#{}", id))
-    }
-
-    pub fn get_mut(&mut self, id: Id, context: &EffectContext) -> &mut Unit {
-        self.units
-            .get_mut(&id)
-            .or(self.dead_units.get_mut(&id))
-            .expect(&format!("Can't find Unit#{}", id))
     }
 }
 
