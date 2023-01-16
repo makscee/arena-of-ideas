3ce09de4e776b380288452a5ff272cb3ab133210
Maksim Chugunov <shadeoflance@gmail.com>
2023-01-16 15:47:41 +0300

    Unit shader fixes

--- static/shaders/system/unit.glsl
+++ static/shaders/system/unit.glsl
@@ -6,10 +6,11 @@ attribute vec2 a_pos;
 uniform mat3 u_projection_matrix;
 uniform mat3 u_view_matrix;
 
+uniform float u_padding;
 uniform float u_size;
 
 void main() {
-    v_quad_pos = a_pos * 1.0;
+    v_quad_pos = a_pos * (1.0 + u_padding);
     vec2 pos = v_quad_pos * 1.0 * u_size;
     vec3 p_pos = u_projection_matrix * u_view_matrix * vec3(pos, 1.0);
     gl_Position = vec4(p_pos.xy, 0.0, p_pos.z);
@@ -19,10 +20,17 @@ void main() {
 #ifdef FRAGMENT_SHADER
 in vec2 v_quad_pos;
 
+const float THICKNESS = 0.03;
+const float SPREAD = 0.1;
+const float GLOW = 0.2;
+
 void main() {
-    vec4 color = u_color;
-    color = vec4(0.6);
-    color = vec4(player_faction_color, 1);
+    float len = abs(1. - length(v_quad_pos));
+    if(len > THICKNESS + SPREAD)
+        discard;
+    commonInit();
+    float alpha = max(smoothstep(THICKNESS, THICKNESS * .5, len), GLOW * smoothstep(THICKNESS + SPREAD, THICKNESS, len));
+    vec4 color = vec4(parent_faction_color, alpha);
     gl_FragColor = color;
 }
 #endif
