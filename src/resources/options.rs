395aa3626378706c3ce926e5e1c16ce29833413b
Maksim Chugunov <shadeoflance@gmail.com>
2023-03-24 21:01:02 +0300

    Add Cheater floor

--- src/resources/options.rs
+++ src/resources/options.rs
@@ -15,6 +15,7 @@ pub struct Options {
 impl FileWatcherLoader for Options {
     fn loader(resources: &mut Resources, path: &PathBuf, watcher: &mut FileWatcherSystem) {
         resources.options = Self::load();
+        resources.transition_state = GameState::MainMenu;
     }
 }
 
