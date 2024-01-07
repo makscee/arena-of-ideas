5f3020922b221760ebf2dc87c9e0f294d74d42e7
Maksim Chugunov <shadeoflance@gmail.com>
2024-01-07 09:19:59 +0300

    Fix global tower number

--- server/src/global_tower.rs
+++ server/src/global_tower.rs
@@ -3,7 +3,6 @@ use super::*;
 #[spacetimedb(table)]
 pub struct GlobalTower {
     #[primarykey]
-    #[autoinc]
     number: u64,
     owner: Option<u64>,
     floor: TowerFloor,
@@ -18,13 +17,14 @@ pub enum TowerFloor {
 #[spacetimedb(reducer)]
 fn extend_global_tower(ctx: ReducerContext, enemy: String, team: String) -> Result<(), String> {
     let user = User::find_by_identity(&ctx.sender)?;
+    let number = GlobalTower::iter().count() as u64;
     GlobalTower::insert(GlobalTower {
-        number: 0,
+        number: number + 1,
         owner: None,
         floor: TowerFloor::Enemy(enemy),
     })?;
     GlobalTower::insert(GlobalTower {
-        number: 0,
+        number: number + 2,
         owner: Some(user.id),
         floor: TowerFloor::Team(team),
     })?;
@@ -34,17 +34,17 @@ fn extend_global_tower(ctx: ReducerContext, enemy: String, team: String) -> Resu
 impl GlobalTower {
     pub fn init() -> Result<(), String> {
         GlobalTower::insert(GlobalTower {
-            number: 0,
+            number: 1,
             owner: None,
             floor: TowerFloor::Enemy("Bug_1".to_owned()),
         })?;
         GlobalTower::insert(GlobalTower {
-            number: 0,
+            number: 2,
             owner: None,
             floor: TowerFloor::Enemy("Bug_5".to_owned()),
         })?;
         GlobalTower::insert(GlobalTower {
-            number: 0,
+            number: 3,
             owner: None,
             floor: TowerFloor::Enemy("Snake_2".to_owned()),
         })?;
