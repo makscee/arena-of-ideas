68acbbf2ce0733c0540a1a5c218525512ce79f49
Maksim Chugunov <shadeoflance@gmail.com>
2025-02-10 07:26:08 +0300

    Reroll btn; Show g balance;

--- src/plugins/match.rs
+++ src/plugins/match.rs
@@ -31,10 +31,66 @@ impl MatchPlugin {
 
         let shop_case = m.shop_case;
         world.insert_resource(MatchData {
-            g: 13,
+            g: m.g,
             shop_case,
             team_world,
             core_world,
+        });
+    }
+    fn show_shop_case(md: &mut MatchData, ui: &mut Ui) {
+        let shop_slots = md.shop_case.len();
+        ui.columns(shop_slots, |ui| {
+            for i in 0..shop_slots {
+                let ui = &mut ui[i];
+                ui.with_layout(
+                    Layout::bottom_up(Align::Center).with_cross_justify(true),
+                    |ui| {
+                        let sc = &md.shop_case[i];
+                        let Some(entity) = md.core_world.get_id_link(sc.unit_id) else {
+                            return;
+                        };
+
+                        if format!("[b buy [yellow {}g]]", global_settings().match_g.unit_buy)
+                            .as_button()
+                            .enabled(!sc.sold)
+                            .ui(ui)
+                            .clicked()
+                        {
+                            cn().reducers.match_buy(i as u8).unwrap();
+                        }
+                        let context = &Context::new_world(&md.core_world).set_owner(entity).take();
+                        let name = context.get_string(VarName::name).unwrap();
+                        let color = context.get_color(VarName::color).unwrap();
+                        TagWidget::new_text(name, if sc.sold { VISIBLE_DARK } else { color })
+                            .ui(ui);
+                        let size = ui.available_size();
+                        let size = size.x.at_most(size.y);
+                        let rect = ui
+                            .allocate_ui_at_rect(
+                                Rect::from_center_size(
+                                    ui.available_rect_before_wrap().center(),
+                                    egui::vec2(size, size),
+                                ),
+                                |ui| show_slot(i, 1, false, ui).rect,
+                            )
+                            .inner
+                            .shrink(10.0);
+                        if !sc.sold {
+                            RepresentationPlugin::paint_rect(
+                                rect,
+                                context,
+                                &unit_rep().material,
+                                ui,
+                            )
+                            .log();
+                            if let Some(rep) = md.core_world.get::<Representation>(entity) {
+                                RepresentationPlugin::paint_rect(rect, context, &rep.material, ui)
+                                    .log();
+                            }
+                        }
+                    },
+                );
+            }
         });
     }
     pub fn open_shop_window(world: &mut World) {
@@ -44,7 +100,6 @@ impl MatchPlugin {
         }
         Window::new("Match", move |ui, world| {
             let mut md = world.remove_resource::<MatchData>().unwrap();
-            let shop_slots = md.shop_case.len();
             let full_rect = ui.available_rect_before_wrap();
             ui.horizontal(|ui| {
                 ui.vertical(|ui| {
@@ -70,76 +125,45 @@ impl MatchPlugin {
                                     ),
                                 )
                                 .ui(ui);
-                                if "sell".cstr_s(CstrStyle::Bold).button(ui).clicked() {
+                                if format!(
+                                    "[b sell [yellow +{}g]]",
+                                    global_settings().match_g.unit_sell
+                                )
+                                .button(ui)
+                                .clicked()
+                                {
                                     cn().reducers.match_sell(unit.name.clone()).unwrap();
                                 }
                             });
                     }
                     ui.expand_to_include_y(full_rect.bottom());
                 });
-                ui.columns(shop_slots, |ui| {
-                    for i in 0..shop_slots {
-                        let ui = &mut ui[i];
-                        ui.with_layout(
-                            Layout::bottom_up(Align::Center).with_cross_justify(true),
-                            |ui| {
-                                let sc = &md.shop_case[i];
-                                let Some(entity) = md.core_world.get_id_link(sc.unit_id) else {
-                                    return;
-                                };
-
-                                if "buy"
-                                    .cstr_s(CstrStyle::Bold)
-                                    .as_button()
-                                    .enabled(!sc.sold)
-                                    .ui(ui)
-                                    .clicked()
-                                {
-                                    cn().reducers.match_buy(i as u8).unwrap();
-                                }
-                                let context =
-                                    &Context::new_world(&md.core_world).set_owner(entity).take();
-                                let name = context.get_string(VarName::name).unwrap();
-                                let color = context.get_color(VarName::color).unwrap();
-                                TagWidget::new_text(
-                                    name,
-                                    if sc.sold { VISIBLE_DARK } else { color },
+                let rect = ui.available_rect_before_wrap();
+                ui.allocate_ui_at_rect(
+                    rect.with_max_y(rect.bottom() - rect.height() * 0.5),
+                    |ui| {
+                        Self::show_shop_case(&mut md, ui);
+                    },
+                );
+                ui.allocate_ui_at_rect(
+                    rect.with_min_y(rect.top() + rect.height() * 0.5 + 5.0),
+                    |ui| {
+                        ui.vertical(|ui| {
+                            ui.horizontal(|ui| {
+                                format!("[yellow [h2 {}g]]", md.g).label(ui);
+                                if format!(
+                                    "[b reroll [yellow {}g]]",
+                                    global_settings().match_g.reroll
                                 )
-                                .ui(ui);
-                                let size = ui.available_size();
-                                let size = size.x.at_most(size.y);
-                                let rect = ui
-                                    .allocate_ui_at_rect(
-                                        Rect::from_center_size(
-                                            ui.available_rect_before_wrap().center(),
-                                            egui::vec2(size, size),
-                                        ),
-                                        |ui| show_slot(i, 1, false, ui).rect,
-                                    )
-                                    .inner
-                                    .shrink(10.0);
-                                if !sc.sold {
-                                    RepresentationPlugin::paint_rect(
-                                        rect,
-                                        context,
-                                        &unit_rep().material,
-                                        ui,
-                                    )
-                                    .log();
-                                    if let Some(rep) = md.core_world.get::<Representation>(entity) {
-                                        RepresentationPlugin::paint_rect(
-                                            rect,
-                                            context,
-                                            &rep.material,
-                                            ui,
-                                        )
-                                        .log();
-                                    }
+                                .button(ui)
+                                .clicked()
+                                {
+                                    cn().reducers.match_reroll().unwrap();
                                 }
-                            },
-                        );
-                    }
-                });
+                            });
+                        })
+                    },
+                )
             });
             for (entity, slot) in md
                 .team_world
