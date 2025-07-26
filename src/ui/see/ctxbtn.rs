ad2b3fe143b89e6a03884a58a9ff84bed88626ee
Maksim Chugunov <shadeoflance@gmail.com>
2025-07-26 18:57:01 +0300

    CtxBtn support node publishing

--- src/ui/see/ctxbtn.rs
+++ src/ui/see/ctxbtn.rs
@@ -9,19 +9,21 @@ pub enum CtxBtnAction<T> {
 
 pub struct CtxBtnBuilder<'a, T: Clone> {
     item: &'a T,
-    copy_action: Option<Box<dyn FnOnce(T) + 'a>>,
+    context: &'a Context<'a>,
+    copy_action: Option<Box<dyn FnOnce(T, &Context) + 'a>>,
     paste_fn: Option<Box<dyn Fn() -> Option<T> + 'a>>,
-    copy_node_full_action: Option<Box<dyn FnOnce(T) + 'a>>,
+    copy_node_full_action: Option<Box<dyn FnOnce(T, &Context) + 'a>>,
     paste_node_full_fn: Option<Box<dyn Fn() -> Option<T> + 'a>>,
     enable_delete: bool,
-    custom_actions: Vec<(String, Box<dyn FnOnce(T) + 'a>)>,
-    custom_dangerous_actions: Vec<(String, Box<dyn FnOnce(T) + 'a>)>,
+    custom_actions: Vec<(String, Box<dyn FnOnce(T, &Context) + 'a>)>,
+    custom_dangerous_actions: Vec<(String, Box<dyn FnOnce(T, &Context) + 'a>)>,
 }
 
 impl<'a, T: Clone> CtxBtnBuilder<'a, T> {
-    pub fn new(item: &'a T) -> Self {
+    pub fn new(item: &'a T, context: &'a Context<'a>) -> Self {
         Self {
             item,
+            context,
             copy_action: None,
             paste_fn: None,
             copy_node_full_action: None,
@@ -36,7 +38,7 @@ impl<'a, T: Clone> CtxBtnBuilder<'a, T> {
     where
         T: StringData,
     {
-        self.copy_action = Some(Box::new(|item| {
+        self.copy_action = Some(Box::new(|item, _context| {
             clipboard_set(item.get_data());
         }));
         self
@@ -44,7 +46,7 @@ impl<'a, T: Clone> CtxBtnBuilder<'a, T> {
 
     pub fn on_copy<F>(mut self, f: F) -> Self
     where
-        F: FnOnce(T) + 'a,
+        F: FnOnce(T, &Context) + 'a,
     {
         self.copy_action = Some(Box::new(f));
         self
@@ -77,9 +79,82 @@ impl<'a, T: Clone> CtxBtnBuilder<'a, T> {
         self
     }
 
+    pub fn add_publish(mut self) -> Self
+    where
+        T: Node,
+    {
+        self.custom_actions.push((
+            "📤 Publish".to_string(),
+            Box::new(|item, _context| {
+                let mut pack = PackedNodes::default();
+                pack.root = item.id();
+                item.pack_fill(&mut pack);
+
+                op(move |world| {
+                    Window::new("Publish Node", move |ui, world| {
+                        if "Publish".cstr().button(ui).clicked() {
+                            cn().reducers
+                                .content_publish_node(to_ron_string(&pack))
+                                .unwrap();
+                            WindowPlugin::close_current(world);
+                        }
+                        Context::from_world(world, |context| {
+                            pack.kind()
+                                .to_kind()
+                                .view_pack_with_children_mut(context, ui, &mut pack)
+                                .ui(ui);
+                        });
+                    })
+                    .push(world);
+                });
+            }),
+        ));
+        self
+    }
+
+    pub fn add_publish_nested(mut self) -> Self
+    where
+        T: Node,
+    {
+        self.custom_actions.push((
+            "📦 Publish Nested".to_string(),
+            Box::new(|item, context| {
+                let mut pack = if let Some(entity) = item.get_entity() {
+                    if let Ok(packed_item) = T::pack_entity(context, entity) {
+                        packed_item.pack()
+                    } else {
+                        item.pack()
+                    }
+                } else {
+                    item.pack()
+                };
+
+                op(move |world| {
+                    Window::new("Publish Node Nested", move |ui, world| {
+                        if "Publish".cstr().button(ui).clicked() {
+                            cn().reducers
+                                .content_publish_node(to_ron_string(&pack))
+                                .unwrap();
+                            WindowPlugin::close_current(world);
+                        }
+                        Context::from_world(world, |context| {
+                            pack.kind()
+                                .to_kind()
+                                .view_pack_with_children_mut(context, ui, &mut pack)
+                                .ui(ui);
+                        });
+                    })
+                    .expand()
+                    .push(world);
+                });
+            }),
+        ));
+        self
+    }
+
     pub fn add_action<F>(mut self, name: String, f: F) -> Self
     where
-        F: FnOnce(T) + 'a,
+        F: FnOnce(T, &Context) + 'a,
     {
         self.custom_actions.push((name, Box::new(f)));
         self
@@ -87,19 +162,17 @@ impl<'a, T: Clone> CtxBtnBuilder<'a, T> {
 
     pub fn add_dangerous_action<F>(mut self, name: String, f: F) -> Self
     where
-        F: FnOnce(T) + 'a,
+        F: FnOnce(T, &Context) + 'a,
     {
         self.custom_dangerous_actions.push((name, Box::new(f)));
         self
     }
 
-    pub fn add_copy_node_full(mut self, context: &'a Context<'a>) -> Self
+    pub fn add_copy_node_full(mut self) -> Self
     where
         T: Node,
     {
-        let context_ptr = context as *const Context<'a>;
-        self.copy_node_full_action = Some(Box::new(move |item| {
-            let context = unsafe { &*context_ptr };
+        self.copy_node_full_action = Some(Box::new(|item, context| {
             if let Some(entity) = item.get_entity() {
                 if let Ok(packed) = T::pack_entity(context, entity) {
                     let packed_nodes = packed.pack();
@@ -160,14 +233,14 @@ impl<'a, T: Clone> CtxBtnBuilder<'a, T> {
 
                     if let Some(copy_fn) = self.copy_action.take() {
                         if ui.button("📋 Copy").clicked() {
-                            copy_fn(self.item.clone());
+                            copy_fn(self.item.clone(), self.context);
                             ui.close_menu();
                         }
                     }
 
                     if let Some(copy_node_full_fn) = self.copy_node_full_action.take() {
                         if ui.button("📦 Copy Node Full").clicked() {
-                            copy_node_full_fn(self.item.clone());
+                            copy_node_full_fn(self.item.clone(), self.context);
                             ui.close_menu();
                         }
                     }
@@ -192,7 +265,7 @@ impl<'a, T: Clone> CtxBtnBuilder<'a, T> {
 
                     for (name, action) in self.custom_actions {
                         if ui.button(&name).clicked() {
-                            action(self.item.clone());
+                            action(self.item.clone(), self.context);
                             ui.close_menu();
                             break;
                         }
@@ -210,7 +283,7 @@ impl<'a, T: Clone> CtxBtnBuilder<'a, T> {
                             )
                             .clicked()
                         {
-                            action(self.item.clone());
+                            action(self.item.clone(), self.context);
                             ui.close_menu();
                             break;
                         }
@@ -278,7 +351,7 @@ impl<T> CtxBtnResponse<T> {
 
 impl<'a, T: Clone + SFnTitle> SeeBuilder<'a, T> {
     pub fn ctxbtn(self) -> CtxBtnBuilder<'a, T> {
-        CtxBtnBuilder::new(self.data())
+        CtxBtnBuilder::new(self.data(), self.context())
     }
 }
 
@@ -287,14 +360,14 @@ impl<'a, T: NodeExt + Clone + SFnTitle> SeeBuilder<'a, T> {
     where
         T: StringData + Default,
     {
-        CtxBtnBuilder::new(self.data()).with_node_defaults()
+        CtxBtnBuilder::new(self.data(), self.context()).with_node_defaults()
     }
 
     pub fn node_ctxbtn_full(self) -> CtxBtnBuilder<'a, T>
     where
         T: StringData + Default + Node,
     {
-        CtxBtnBuilder::new(self.data()).with_node_full_defaults(self.context())
+        CtxBtnBuilder::new(self.data(), self.context()).with_node_full_defaults()
     }
 }
 
@@ -306,14 +379,16 @@ impl<'a, T: NodeExt + Clone> CtxBtnBuilder<'a, T> {
         self.add_copy().with_paste().with_delete()
     }
 
-    pub fn with_node_full_defaults(self, context: &'a Context<'a>) -> Self
+    pub fn with_node_full_defaults(self) -> Self
     where
         T: Node + StringData + Default,
     {
         self.add_copy()
             .with_paste()
-            .add_copy_node_full(context)
+            .add_copy_node_full()
             .add_paste_node_full()
+            .add_publish()
+            .add_publish_nested()
             .with_delete()
     }
 }
