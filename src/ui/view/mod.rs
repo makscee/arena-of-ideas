c10fb395df2f5b948cd31c6ad7e6cd05ff2ef418
Maksim Chugunov <shadeoflance@gmail.com>
2025-08-18 12:17:47 +0300

    Separate client crate

--- src/ui/view/mod.rs
+++
@@ -1,9 +0,0 @@
-mod view;
-mod view_impl;
-mod view_node;
-
-pub use super::*;
-
-pub use view::*;
-
-pub use view_node::*;
--- src/utils/mod.rs
+++
@@ -1,7 +0,0 @@
-use super::*;
-
-mod entity_ext;
-mod r#fn;
-
-pub use entity_ext::*;
-pub use r#fn::*;
